eef7446320729e8cf2d23d60b59cc02acbe58554
Goldorion <38427877+Goldorion@users.noreply.github.com>
2024-02-25 22:42:35 -0500

    Big code refactoring + Potion items support

--- src/main/java/net/goldorion/workspace2api/GeneratePluginAction.java
+++ src/main/java/net/goldorion/workspace2api/GeneratePluginAction.java
@@ -14,7 +14,8 @@ import java.util.*;
 
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public class GeneratePluginAction extends BasicAction {
-    private static final String[] SUPPORTED_GENERATORS = { "forge-1.19.4", "forge-1.20,1", "forge-1.20.4" };
+    private static final String[] SUPPORTED_GENERATORS = {"datapack-1.19.4", "datapack-1.20.1", "forge-1.19.4", "forge-1.20,1", "forge-1.20.4"};
+    private static final File FOLDER = new File(FileUtils.getTempDirectory(), "workspace2plugin" + new Random().nextInt(1000000) + "/");
 
     public GeneratePluginAction(ActionRegistry actionRegistry) {
         super(actionRegistry, L10N.t("action.workspace2api.generate_plugin"), e -> {
@@ -22,92 +23,55 @@ public class GeneratePluginAction extends BasicAction {
             String modID = workspace.getWorkspaceSettings().getModID();
             String modName = workspace.getWorkspaceSettings().getModName().replace(" ", "") + "/";
             String javaModName = workspace.getWorkspaceSettings().getJavaModName();
-            File destFolder = new File(FileUtils.getTempDirectory(), "workspace2plugin" + new Random().nextInt(1000000) + "/");
             try {
-                destFolder.mkdirs();
-                destFolder.createNewFile();
-                System.out.println(destFolder.getPath());
+                FOLDER.mkdirs();
+                FOLDER.createNewFile();
+                System.out.println(FOLDER.getPath());
             } catch (IOException ex) {
                 Workspace2API.LOG.error("Error while creating the temp directory. Stopping the generation of the plugin.", ex);
                 return;
             }
 
-            // Generate advancement entries
-            List<String> advancementsDL = new ArrayList<>();
-            Map<String, String> advancementsMappings = new HashMap<>();
-
-            workspace.getModElements().forEach(me -> {
-                if (me.getType() == ModElementType.ADVANCEMENT) {
-                    advancementsDL.add(modName + me.getName());
-                    advancementsMappings.put(modName + me.getName(),  modID + ":" + me.getRegistryName());
-                }
-            });
-            if (!advancementsDL.isEmpty()) {
-                YamlWriter.writeObject(advancementsDL, new File(destFolder, "datalists/advancements.yaml"));
-                Arrays.stream(SUPPORTED_GENERATORS).forEach(gen ->  YamlWriter.writeObject(advancementsMappings, new File(destFolder, gen + "/mappings/advancements.yaml")));
-            }
-
-            // Generate biomes entries
-            List<String> biomesDL = new ArrayList<>();
-            Map<String, String> biomesMappings = new HashMap<>();
-
-            workspace.getModElements().forEach(me -> {
-                if (me.getType() == ModElementType.BIOME) {
-                    biomesDL.add(modName + me.getName());
-                    biomesMappings.put(modName + me.getName(),  modID + ":" + me.getRegistryName());
-                }
-            });
-            if (!biomesDL.isEmpty()) {
-                YamlWriter.writeObject(biomesDL, new File(destFolder, "datalists/biomes.yaml"));
-                Arrays.stream(SUPPORTED_GENERATORS).forEach(gen ->  YamlWriter.writeObject(biomesMappings, new File(destFolder, gen + "/mappings/biomes.yaml")));
-            }
-
-            // Generate potion effects entries
-            List<String> effectsDL = new ArrayList<>();
-            Map<String, String> effectsMappings = new HashMap<>();
-
-            workspace.getModElements().forEach(me -> {
-                if (me.getType() == ModElementType.POTIONEFFECT) {
-                    effectsDL.add(modName + me.getName());
-                    effectsMappings.put(modName + me.getName(),  javaModName + "Effects." + me.getRegistryName().toUpperCase(Locale.ENGLISH) + ".get()");
-                }
-            });
-            if (!effectsDL.isEmpty()) {
-                YamlWriter.writeObject(effectsDL, new File(destFolder, "datalists/effects.yaml"));
-                Arrays.stream(SUPPORTED_GENERATORS).forEach(gen ->  YamlWriter.writeObject(effectsMappings, new File(destFolder, gen + "/mappings/effects.yaml")));
-            }
-
-            // Generate damage sources entries
-            List<String> damagesDL = new ArrayList<>();
-            Map<String, String> damagesMappings = new HashMap<>();
-
-            workspace.getModElements().forEach(me -> {
-                if (me.getType() == ModElementType.DAMAGETYPE) {
-                    damagesDL.add(modName + me.getName());
-                    damagesMappings.put(modName + me.getName(),  modID + ":" + me.getRegistryName());
-                }
-            });
-            if (!damagesDL.isEmpty()) {
-                YamlWriter.writeObject(damagesDL, new File(destFolder, "datalists/damagesources.yaml"));
-                Arrays.stream(SUPPORTED_GENERATORS).forEach(gen ->  YamlWriter.writeObject(damagesMappings, new File(destFolder, gen + "/mappings/damagesources.yaml")));
-            }
-
-            // Generate enchantments entries
-            List<String> enchantmentsDL = new ArrayList<>();
-            Map<String, String> enchantmentsMappings = new HashMap<>();
-
-            workspace.getModElements().forEach(me -> {
-                if (me.getType() == ModElementType.ENCHANTMENT) {
-                    enchantmentsDL.add(modName + me.getName());
-                    enchantmentsMappings.put(modName + me.getName(),  javaModName + "Enchantments." + me.getRegistryName().toUpperCase(Locale.ENGLISH) + ".get()");
-                }
-            });
-            if (!enchantmentsDL.isEmpty()) {
-                YamlWriter.writeObject(enchantmentsDL, new File(destFolder, "datalists/enchantments.yaml"));
-                Arrays.stream(SUPPORTED_GENERATORS).forEach(gen ->  YamlWriter.writeObject(enchantmentsMappings, new File(destFolder, gen + "/mappings/enchantments.yaml")));
-            }
-
+            generateJSONElement(workspace, ModElementType.ADVANCEMENT, "advancements", modName, modID);
+            generateJSONElement(workspace, ModElementType.BIOME, "biomes", modName, modID);
+            generateJavaElement(workspace, ModElementType.POTIONEFFECT, "Effects", "effects", modName, javaModName);
+            generateJSONElement(workspace, ModElementType.DAMAGETYPE, "damagesources", modName, modID);
+            generateJavaElement(workspace, ModElementType.ENCHANTMENT, "Enchantments", "enchantments", modName, javaModName);
+            generateJavaElement(workspace, ModElementType.POTION, "Potions", "potions", modName, javaModName);
 
         });
+    }
+
+    private static void generateJSONElement(Workspace workspace, ModElementType<?> met, String fileName, String modName, String modID) {
+        List<String> dataList = new ArrayList<>();
+        Map<String, String> mappings = new HashMap<>();
+
+        workspace.getModElements().forEach(me -> {
+            if (me.getType() == met) {
+                dataList.add(modName + me.getName());
+                mappings.put(modName + me.getName(), modID + ":" + me.getRegistryName());
+            }
+        });
+        if (!dataList.isEmpty()) {
+            YamlWriter.writeObject(dataList, new File(FOLDER, "datalists/" + fileName + ".yaml"));
+            Arrays.stream(SUPPORTED_GENERATORS).filter(name -> name.startsWith("datapack")).forEach(name ->
+                    YamlWriter.writeObject(mappings, new File(FOLDER, name + "/mappings/" + fileName + ".yaml")));
+        }
+    }
+
+    private static void generateJavaElement(Workspace workspace, ModElementType<?> met, String className, String fileName, String modName, String javaModName) {
+        List<String> dataList = new ArrayList<>();
+        Map<String, String> mappings = new HashMap<>();
+
+        workspace.getModElements().forEach(me -> {
+            if (me.getType() == met) {
+                dataList.add(modName + me.getName());
+                mappings.put(modName + me.getName(), javaModName + className + "." + me.getRegistryName().toUpperCase(Locale.ENGLISH) + ".get()");
+            }
+        });
+        if (!dataList.isEmpty()) {
+            YamlWriter.writeObject(dataList, new File(FOLDER, "datalists/" + fileName + ".yaml"));
+            Arrays.stream(SUPPORTED_GENERATORS).filter(name -> !name.startsWith("datapack")).forEach(gen -> YamlWriter.writeObject(mappings, new File(FOLDER, gen + "/mappings/" + fileName + ".yaml")));
+        }
     }
 }
